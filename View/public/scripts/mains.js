f0ef18224f8b86a6801bb7bd5d840d435094526f
amitha511 <amitha51111@gmail.com>
2022-08-11 15:08:00 +0300

    chat

--- View/public/scripts/mains.js
+++ View/public/scripts/mains.js
@@ -1,4 +1,4 @@
-function preLoad(){
+function preLoad() {
     $(document).ready(function () {
         $("#bodyPageHome").empty();
         $("#com").empty();
@@ -24,11 +24,11 @@ function ShowChatPage() {
 
     });
 }
- 
+
 function ShowSearchPage() {
     $(document).ready(function () {
         preLoad();
-        $("#com").load("search.html");  
+        $("#com").load("search.html");
         console.log("load search page");
     });
 }
@@ -44,7 +44,7 @@ function ShowCatalogPage() {
 function ShowLogInPage() {
     $(document).ready(function () {
         preLoad();
-     //   $("#com").load("login.html");   need to build html
+        //   $("#com").load("login.html");   need to build html
         console.log("load Log In page");
 
     });
@@ -53,7 +53,7 @@ function ShowLogInPage() {
 function ShowSingnupPage() {
     $(document).ready(function () {
         preLoad();
-       // $("#com").load("signup.html");   need to build html
+        // $("#com").load("signup.html");   need to build html
         console.log("load Sign up page");
 
     });
@@ -68,14 +68,13 @@ function ShowCartPage() {
     });
 }
 // Filter number 1 - by gender, size, category
+// Filter number 1 - by gender, size, category
 function ShowCatalog1(val1, val2, val3) {
 
     $.get("catalogarr", function (data, status) {
-        console.log("load page1");
         $("#company").empty();
         $("#price").empty();
         let count = 0;
-        console.log("load page2");
         let UserGender = val1
         let UserSize = val2;
         let UserCategory = val3;
@@ -85,10 +84,12 @@ function ShowCatalog1(val1, val2, val3) {
         for (let i = 0; i < data.length; i++) {
             if ((UserSize == data[i].size || UserSize == "all") && (UserGender == data[i].gender || UserGender == "all") && (UserCategory == data[i].category || UserCategory == "all")) {
                 $("#company").append(`<p id="proname"><b><Company:</b></font> ${data[i].company}</p> <p><b>Price:</b>${data[i].price}$</p>
-                        <p><b>Color: </b>${data[i].color}</p>
-                        <p><b>Size: </b>${data[i].size}</p>
-                        <p><b>Gender: </b>${data[i].gender}</p>
-                        <p><b>Category: </b>${data[i].category}</p>`);
+                <p><b>Color: </b>${data[i].color}</p>
+                <p><b>Size: </b>${data[i].size}</p>
+                <p><b>Gender: </b>${data[i].gender}</p>
+                <p><b>Category: </b>${data[i].category}</p>`);
+                var col = document.getElementById('company');
+                col.innerHTML += '<button onclick="AddCart(' + i + ')">Add To Cart</button>';
                 document.getElementById("price").innerHTML += `<p><img id="image_` + i + `" class="pic"/></p><br><br><br>`;
                 $("#image" + "_" + i).attr("src", data[i].imageUrl);
                 $("#image" + "_" + i).attr("width", "300px");
@@ -103,5 +104,65 @@ function ShowCatalog1(val1, val2, val3) {
     });
 
 }
+function ShowCatalog2(val1, val2, val3) {
+    $.get("catalogarr", function (data, status) {
+        $("#company").empty();
+        $("#price").empty();
+        console.log(val1);
+        console.log(val2);
+        console.log(val3);
+        let count = 0;
+        let UserCompany = val1;
+        let UserPrice = val2;
+        let UserColor = val3;
+        for (let i = 0; i < data.length; i++) {
+            if ((UserPrice >= data[i].price || UserPrice == "all") && (UserCompany == data[i].company || UserCompany == "all") && (UserColor == data[i].color || UserColor == "all")) {
+                $("#company").append(`<p id="proname"><b><Company:</b></font> ${data[i].company}</p> <p><b>Price:</b>${data[i].price}$</p>
+                <p><b>Color: </b>${data[i].color}</p>
+                <p><b>Size: </b>${data[i].size}</p>
+                <p><b>Gender: </b>${data[i].gender}</p>
+                <p><b>Category: </b>${data[i].category}</p>`);
+                var col = document.getElementById('company');
+                col.innerHTML += '<button onclick="AddCart(' + i + ')">Add To Cart</button>';
+                document.getElementById("price").innerHTML += `<p><img id="image_` + i + `" class="pic"/></p><br><br><br>`;
+                $("#image" + "_" + i).attr("src", data[i].imageUrl);
+                $("#image" + "_" + i).attr("width", "300px");
+                $("#image" + "_" + i).attr("height", "195px");
+                count++;
+            }
+        }
+        console.log(count);
+        if (count == 0) {
+            $("#company").append("Sorry! There are 0 results to your search. Please try again!");
+        }
+
+    });
+
+}
+function AddCart(i) {
+    let index = i;
+    console.log(i);
+    $.get("catalogarr", function (data, status) {
+        console.log(i);
+        document.getElementById("com").getElementsByClassName("cart-product-title")[0].innerHTML += `<p id="proname"><b><Company:</b></font> ${data[i].company}</p> <p><b>Price:</b>${data[i].price}$</p>
+        <p><b>Color: </b>${data[i].color}</p>
+        <p><b>Size: </b>${data[i].size}</p>
+        <p><b>Gender: </b>${data[i].gender}</p>
+        <p><b>Category: </b>${data[i].category}</p>`;
+        document.getElementById("com").getElementsByClassName("cart-product-title")[0].innerHTML += `<p><img id="img_` + i + `" class="pic1"/></p>`;
+        $("#img" + "_" + i).attr("src", data[i].imageUrl);
+        $("#img" + "_" + i).attr("width", "150px");
+        $("#img" + "_" + i).attr("height", "100px");
+        document.getElementById("com").getElementsByClassName("cart-product-title")[0].innerHTML += `<button id="botton_` + i + `" onclick="Remove(` + i + `)">Remove</button><br><br><br>`;
+    });
+
+}
+function Remove(i) {
+    $("com").empty();
+
+}
+
+
+
 
 
